348f2cee7a492ab6dadc3b9fd249ce1ccfff696b
Susan <suusjevanglabbeek@live.nl>
2024-06-04 16:05:26 +0200

    erased files

--- posetracking_socket/app/config.py
+++
@@ -1,5 +0,0 @@
-from cvzone.HandTrackingModule import HandDetector
-import mediapipe as mp
-
-hand_detector = HandDetector(detectionCon=0.8, maxHands=2)
-pose_detector = mp.solutions.pose.Pose(static_image_mode=True, min_detection_confidence=0.5)
